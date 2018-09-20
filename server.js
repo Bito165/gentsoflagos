60f850a31cd3981728ab701b7334535cc2911be0
Babalola Fareed <kbdgreat165@yahoo.com>
2018-09-20 18:41:55 +0100

    Comitting change to credentials

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://gentsoflagos.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'gentsoflagos.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
